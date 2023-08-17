f1ff129ddedfe57ab772bef37e1cb86c3fb5372c
Ponrawoot <minnaja853@gmail.com>
2023-08-17 11:32:59 +0700

    add files

---
+++ register.js
@@ -0,0 +1,20 @@
+function validateForm() {
+    var companyName = document.getElementById("companyName").value;
+    var numChairs = document.getElementById("numChairs").value;
+    var contactName = document.getElementById("contactName").value;
+    var contactNumber = document.getElementById("contactNumber").value;
+
+    if (companyName === "" || numChairs === "" || contactName === "" || contactNumber === "" ) {
+        alert("Please fill out all required fields.");
+        return false;
+    }
+
+    if (numChairs < 1 || numChairs > 10) {
+        alert("Please enter a number of chairs between 1 and 10.");
+        return false;
+    }
+
+    setTimeout(function() {
+        window.location.href = "index.html";
+    }, 10);
+}
