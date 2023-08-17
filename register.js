c0f1633645eecbddc777c58606ff4391c5bf83bf
Ponrawoot <minnaja853@gmail.com>
2023-08-17 11:56:58 +0700

    format code

--- register.js
+++ register.js
@@ -1,20 +1,25 @@
 function validateForm() {
-    var companyName = document.getElementById("companyName").value;
-    var numChairs = document.getElementById("numChairs").value;
-    var contactName = document.getElementById("contactName").value;
-    var contactNumber = document.getElementById("contactNumber").value;
+  var companyName = document.getElementById("companyName").value;
+  var numChairs = document.getElementById("numChairs").value;
+  var contactName = document.getElementById("contactName").value;
+  var contactNumber = document.getElementById("contactNumber").value;
 
-    if (companyName === "" || numChairs === "" || contactName === "" || contactNumber === "" ) {
-        alert("Please fill out all required fields.");
-        return false;
-    }
+  if (
+    companyName === "" ||
+    numChairs === "" ||
+    contactName === "" ||
+    contactNumber === ""
+  ) {
+    alert("Please fill out all required fields.");
+    return false;
+  }
 
-    if (numChairs < 1 || numChairs > 10) {
-        alert("Please enter a number of chairs between 1 and 10.");
-        return false;
-    }
+  if (numChairs < 1 || numChairs > 10) {
+    alert("Please enter a number of chairs between 1 and 10.");
+    return false;
+  }
 
-    setTimeout(function() {
-        window.location.href = "index.html";
-    }, 10);
+  setTimeout(function () {
+    window.location.href = "index.html";
+  }, 10);
 }
