ce127addc80b784f42e564983f4307b36fd8fa1b
Ponrawoot <minnaja853@gmail.com>
2023-08-29 16:06:13 +0700

    add new workshop

---
+++ Others/Workshop/car-rental-app/src/app/page.tsx
@@ -0,0 +1,17 @@
+import Banner from "@/components/Banner";
+import ProductCard from "@/components/ProductCard";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+        <Banner/>
+        <div style={{margin:"20px",display:"flex",flexDirection:"row",alignContent:"space-around",justifyContent:"space-around",flexWrap:"wrap"}}>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+        </div>
+    </main>
+  );
+}
