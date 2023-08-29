5610671eb0e82f416f57b76a0f64fa72feb49ace
Ponrawoot <minnaja853@gmail.com>
2023-08-29 16:28:57 +0700

    update product card

--- Others/Workshop/car-rental-app/src/app/page.tsx
+++ Others/Workshop/car-rental-app/src/app/page.tsx
@@ -4,13 +4,13 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
         <Banner/>
         <div style={{margin:"20px",display:"flex",flexDirection:"row",alignContent:"space-around",justifyContent:"space-around",flexWrap:"wrap"}}>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
+          <ProductCard carName="Honda Civic" imgSrc="/img/civic.jpg"/>
+          <ProductCard carName="Honda Accord" imgSrc="/img/accord.jpg"/>
+          <ProductCard carName="Toyota Fortuner" imgSrc="/img/fortuner.jpg"/>
+          <ProductCard carName="Tesla Model 3" imgSrc="/img/tesla.jpg"/>
         </div>
     </main>
   );
