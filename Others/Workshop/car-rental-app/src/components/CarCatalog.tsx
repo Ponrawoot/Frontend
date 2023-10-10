5fe6d537d3f69fe6ccde70828d5a1410a91e19c8
Ponrawoot <minnaja853@gmail.com>
2023-10-10 18:04:34 +0700

    update workshop 9

---
+++ Others/Workshop/car-rental-app/src/components/CarCatalog.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link"
+import ProductCard from "./ProductCard"
+export default async function CarCatalog({carJson} : {carJson:Object}) {
+    const carJsonReady = await carJson
+    return (
+        <>
+        Explore {carJsonReady.count} models in our catalog
+        <div
+        style={{
+          margin: "20px",
+          display: "flex",
+          flexDirection: "row",
+          alignContent: "space-around",
+          justifyContent: "space-around",
+          flexWrap: "wrap",
+        }}
+      >
+        {carJsonReady.data.map((carItem:Object) => (
+          <Link href={`/car/${carItem.id}`} className="w-1/5">
+          <ProductCard
+            carName={carItem.model}
+            imgSrc={carItem.picture}
+          />
+          </Link>
+        ))}
+      </div>
+        </>
+    )
+}
