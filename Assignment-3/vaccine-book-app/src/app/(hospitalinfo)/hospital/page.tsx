3cefba2637bf714d59f0177d8c1450cb7eb42e66
Ponrawoot <minnaja853@gmail.com>
2023-11-01 01:36:58 +0700

    Assignment 12

--- Assignment-3/vaccine-book-app/src/app/(hospitalinfo)/hospital/page.tsx
+++ Assignment-3/vaccine-book-app/src/app/(hospitalinfo)/hospital/page.tsx
@@ -1,9 +1,21 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import AddHospitalForm from "@/components/AddHospitalForm";
 import HospitalCatalog from "@/components/HospitalCatalog";
 import getHospitals from "@/libs/getHospitals";
+import getUserProfile from "@/libs/getUserProfile";
 import { LinearProgress } from "@mui/material";
+import { getServerSession } from "next-auth";
 import { Suspense } from "react";
-export default function Hospital() {
+export default async function Hospital() {
   const hospitals = getHospitals();
+
+  const session = await getServerSession(authOptions);
+
+  let profile = null;
+  if (session) {
+    profile = await getUserProfile(session.user.token);
+  }
+
   return (
     <main className="text-center p-5">
       <Suspense
@@ -14,6 +26,7 @@ export default function Hospital() {
         }
       >
         <HospitalCatalog hospitalJson={hospitals} />
+        {profile?.data?.role == "admin" ? <AddHospitalForm /> : null}
       </Suspense>
     </main>
   );
--- Assignment-3/vaccine-book-app/src/redux/store.ts
+++ Assignment-3/vaccine-book-app/src/redux/store.ts
@@ -1,11 +1,20 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { useSelector, TypedUseSelectorHook } from "react-redux"
 import bookSlice from "./features/bookSlice";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+    key: "rootPersist",
+    storage
+}
+
+const rootPersist = combineReducers({bookSlice})
+const reduxPersistedReducer = persistReducer(persistConfig, rootPersist)
+
 
 export const store = configureStore({
-    reducer:{
-        bookSlice
-    }
+    reducer: reduxPersistedReducer
 })
 
 export type RootState = ReturnType<typeof store.getState>
